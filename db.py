38248167f54b7399309eaa3ed1e7d273847dcf70
byrwinter <41683082+byrwinter@users.noreply.github.com>
2018-07-31 01:16:51 +0100

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 
 
 #variables and settings
-initotClient = MongoClient("mongodb://quentindb:quentin1@ds161411.mlab.com:61411/quentindb")
+initotClient = MongoClient()
 initotdb = initotClient["quentindb"]
 initotadmins = initotdb["initotadmins"]
 initotsubs = initotdb["initotsubs"]
